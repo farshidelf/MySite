a6bac39694e7490e8c81ae7a6f485d857d072286
farshidelf <farshidelf@yahoo.com>
2022-12-31 11:11:09 +0330

    admin page

--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from .models import Question, Choice
 
-# Register your models here.
+admin.site.site_header = 'Farshid Admin'
+
+
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 2
+
+
+class QustionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('Question', {'fields': ['question_text', 'pub_date']})
+    ]
+
+    inlines = ChoiceInline,
+
+    list_display = ('question_text', 'pub_date', 'published_recently')
+
+    list_filter = ('pub_date',)
+    search_fields = ('question_text',)
+
+
+admin.site.register(Question, QustionAdmin)
+admin.site.register(Choice)
